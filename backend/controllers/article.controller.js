1d1bdc65c8bb99c3a297fb602e8509c54df78beb
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2021-02-23 14:54:35 +0100

    backend des articles

---
+++ backend/controllers/article.controller.js
@@ -0,0 +1,98 @@
+const Article = require("../models/article.model.js");
+
+exports.create = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    // Create a Customer
+    const article = new Article({
+        art_nom: req.body.art_nom,
+        cat_id: req.body.cat_id,
+        prix: req.body.prix,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    // Save Customer in the database
+    Article.create(article, (err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Article."
+            });
+        else res.send(data);
+    });
+};
+
+exports.findAll = (req, res) => {
+    Article.getAll((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving articles."
+            });
+        else res.send(data);
+    });
+
+};
+
+exports.findOne = (req, res) => {
+    Article.findById(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Article with id ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Article with id " + req.params.id
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+exports.update = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    Article.updateById(
+        req.params.id,
+        new Article(req.body),
+        (err, data) => {
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Not found Article with id ${req.params.id}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Error updating Article with id " + req.params.id
+                    });
+                }
+            } else res.send(data);
+        }
+    );
+};
+
+exports.delete = (req, res) => {
+    Article.remove(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Customer with id ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Could not delete Customer with id " + req.params.id
+                });
+            }
+        } else res.send({ message: `Customer was deleted successfully!` });
+    });
+};
---
+++ backend/models/article.model.js
@@ -0,0 +1,88 @@
+const sql = require("./db.js");
+
+// constructor
+const Article = function(article) {
+    this.art_nom = article.art_nom;
+    this.cat_id = article.cat_id;
+    this.prix = article.prix;
+    this.image = article.image;
+    this.description = article.description
+};
+
+Article.create = (newArticle, result) => {
+    sql.query("INSERT INTO articles SET ?", newArticle, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        console.log("created article: ", { id: res.insertId, ...newArticle });
+        result(null, { id: res.insertId, ...newArticle });
+    });
+};
+
+Article.findById = (articleId, result) => {
+    sql.query(`SELECT * FROM articles WHERE art_id = ${articleId}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found article: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
+Article.getAll = result => {
+    sql.query("SELECT * FROM articles", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        result(null, res);
+    });
+};
+
+Article.updateById = (id, article, result) => {
+    sql.query(
+        "UPDATE articles SET art_nom = ?, cat_id = ?, prix = ? , description = ? WHERE cat_id = ?",
+        [article.art_nom, article.cat_id, article.prix, article.description, id],
+        (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+                return;
+            }
+
+
+            console.log("updated article: ", { id: id, ...article });
+            result(null, { id: id, ...article });
+        }
+    );
+};
+
+Article.remove = (id, result) => {
+    sql.query("DELETE FROM articles WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        console.log("deleted article with id: ", id);
+        result(null, res);
+    });
+};
+
+
+module.exports = Article;
