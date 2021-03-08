6da53292f591e97d04575af7c670a8a2c962b7a1
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2021-03-08 17:19:40 +0100

    ajout paragraphe descriptif et son css

--- bakery/src/components/accueil/Accueil.js
+++ bakery/src/components/accueil/Accueil.js
@@ -8,7 +8,11 @@ const Accueil = ({element}) => {
         <header className="App-header">
             <img src={banniere} className="App-banniere" alt="banniere" />
             <h1 className="nomBoulangerie">Boulangerie Lonbois</h1>
-            <p>futur paragraphe</p>
+            <p className="b-p-s">Boulangerie - Pâtisserie - Sandwicherie</p>
+            <p className="descriptif">Produits frais, fabriqués dans nos ateliers à Braine l'alleud avec des œufs frais, du vrai beurre et sans huile de palme.
+                Tous nos produits sont fabriqués dans nos ateliers, situés sur la Drève Richelle à Waterloo.
+                Le pétrissage, la fermentation, la mise en forme et la cuisson complète des pains sont fait sur place.
+                Concernant les pâtisseries, celle-ci sont aussi produites dans nos ateliers, avec des œufs frais, du vrai beurre et SANS HUILE DE PALME !</p>
         </header>
     )
 }
--- bakery/src/components/navbar/NavigationBar.js
+++ bakery/src/components/navbar/NavigationBar.js
@@ -24,12 +24,12 @@ export const NavigationBar = () => (
             {/*<Navbar.Brand href="/">Tutorial</Navbar.Brand>*/}
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/commande">Commande</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/connexion">Connexion</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/inscription">Inscription</Nav.Link></Nav.Item>
+                <Nav className="m-md-auto">
+                    <Nav.Item><Nav.Link href="/">|   Home     </Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/commande">|   Commande     </Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/contact">|   Contact     </Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/connexion">|   Connexion     </Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/inscription">|   Inscription     </Nav.Link></Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
