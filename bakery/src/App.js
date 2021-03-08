4ad57be422080f34db64814a010bcd59e1b00e03
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2021-03-08 16:06:36 +0100

    barre de Navigation intégrée

--- bakery/src/App.js
+++ bakery/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import banniere from './boulangerie/banniere.jpg'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/navbar/NavigationBar';
+import React from "react";
+import {About} from "./components/navbar/About";
+import {NoMatch} from "./components/navbar/NoMatch";
 import Home from "./components/home/Home";
+
 
 function App() {
   return (
-   <Home/>
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NoMatch} />
+        </Switch>
+
+        </Router>
+      </React.Fragment>
   );
 }
 
