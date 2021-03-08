c71b8c104b5e406154299354d6b46346bd5bba54
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2021-03-08 15:42:29 +0100

    navbar debut

--- bakery/src/App.js
+++ bakery/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/navbar/NavigationBar';
+import React from "react";
+import {About} from "./components/navbar/About";
+import {NoMatch} from "./components/navbar/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+          <Router>
+              <NavigationBar />              <Switch>
+              <Route path="/about" component={About} />
+              <Route component={NoMatch} />
+          </Switch>
+
+          </Router>
+      </React.Fragment>
   );
 }
 
