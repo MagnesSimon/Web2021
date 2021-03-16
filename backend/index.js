718a2a8137e963bf8e0c55adfe21e26a1cd21c4e
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2021-03-16 13:29:03 +0100

    connexion front-back sur la page commande

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 
 const app = express();
 
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.json() )
+    .use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .use(cors())
+    .use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
 app.get("/", (req, res) => {
     res.json("Bienvenue dans l'API de la boulangerie Lonbois.");
