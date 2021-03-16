08a88b8f0809dcf8d83fefc4cb54332681cb5683
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2021-03-16 23:11:13 +0100

    backend api user

--- backend/routes/article.route.js
+++ backend/routes/article.route.js
@@ -1,19 +1,14 @@
 module.exports = app => {
     const articles = require("../controllers/article.controller.js");
 
-    // Create a new Customer
     app.post("/articles", articles.create);
 
-    // Retrieve all Customers
     app.get("/articles", articles.findAll);
 
-    // Retrieve a single Customer with customerId
     app.get("/articles/:id", articles.findOne);
 
-    // Update a Customer with customerId
     app.put("/articles/:id", articles.update);
 
-    // Delete a Customer with customerId
     app.delete("/articles/:id", articles.delete);
 
 };
