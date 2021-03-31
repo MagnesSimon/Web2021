ddb85f8a598b2c4fc742be6e8d064926b2a37154
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2021-03-31 10:08:47 +0200

    Ajout de la page Panier

---
+++ bakery/src/components/panier/Panier.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = ({element}) => {
+    return(
+        <header className="header-contact">
+            <h1>Page du panier</h1>
+        </header>
+    )
+}
+
+export default Contact
