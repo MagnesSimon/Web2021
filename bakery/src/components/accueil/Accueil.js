00fff73b2d99d2a4b28f127c399dd013e6f7fdf5
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2021-04-20 11:14:41 +0200

    correction titre

--- bakery/src/components/accueil/Accueil.js
+++ bakery/src/components/accueil/Accueil.js
@@ -7,14 +7,14 @@ import Horaire from "./horaire";
 const Accueil = ({element}) => {
     return(
         <div>
-            <h1> Boulangie Lonbois de Waterlot</h1>
+            <h1> Boulangie Lonbois de Braine l'Alleud</h1>
                 <img src={banniere} className="App-banniere" alt="banniere" />
                 <div className='bloc'>
                     <span>Boulangie - </span>
                     <span>Pâtisserie - </span>
                     <span>Sandwicherie</span>
                 </div>
-                <p className="descriptif">Produits frais, fabriqués dans nos ateliers à Braine l'alleud avec des œufs frais, du vrai beurre et sans huile de palme.
+                <p className="descriptif">Produits frais, fabriqués dans nos ateliers à Braine l'Alleud avec des œufs frais, du vrai beurre et sans huile de palme.
                     Tous nos produits sont fabriqués dans nos ateliers, situés sur la Drève Richelle à Waterloo.
                     Le pétrissage, la fermentation, la mise en forme et la cuisson complète des pains sont fait sur place.
                     Concernant les pâtisseries, celle-ci sont aussi produites dans nos ateliers, avec des œufs frais, du vrai beurre et SANS HUILE DE PALME !</p>
